6f03be5737dcbaa8f3de768ad513906a9f16ab2b
kscode <kashinggo@gmail.com>
2022-06-03 16:19:59 +0800

    play, pause & stop

---
+++ script.js
@@ -0,0 +1,51 @@
+const video = document.getElementById("video");
+const play = document.getElementById("play");
+const stop = document.getElementById("stop");
+const progress = document.getElementById("progress");
+const timestamp = document.getElementById("timestamp");
+
+// Play & pause video
+function toggleVideoStatus() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+// Update play/pause icon
+function updatePlayIcon() {
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"</i>';
+  }
+}
+
+// Update progress & timestamp
+function updateProgress() {
+  return true;
+}
+
+// Stop video
+function stopVideo() {
+    video.currentTime = 0;
+    video.pause()
+}
+
+// Set video time
+function setVideoProgress() {
+  return true;
+}
+
+// Event listeners
+video.addEventListener("click", toggleVideoStatus);
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("pause", updatePlayIcon);
+video.addEventListener("timeupdate", updateProgress);
+
+play.addEventListener("click", toggleVideoStatus);
+
+stop.addEventListener("click", stopVideo);
+
+progress.addEventListener("change", setVideoProgress);
